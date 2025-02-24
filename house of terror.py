6ddcef81b2c69f1c36b41a29f4597700fedc0437
serenegeneration <serenegeneration@gmail.com>
2025-02-24 17:14:34 +0000

    Add files via upload

--- house of terror.py
+++ house of terror.py
@@ -8,22 +8,31 @@ rooms = [
 
 ]
 
-def startgame():
-    time.sleep(2)
-    print ("you are travelling down a road in the dead of night.")
-    time.sleep(2)
-    print ("besides you is a forest that stretches as far as the eye can see.")
-    time.sleep(2)
-    print ("suddenly your car breaks down and you quickly get out as smoke pours out of the front of the car.")
-    time.sleep(2)
-    print ("looking around you spot a derelict house.")
-    time.sleep (2)
-    print ("you see only 2 options for what to do in this situation.")
-    time.sleep (2)
-    print ("do you wish to enter the house in hopes of finding help for your predicament?")
-    time.sleep(2)
-    print ("or do you wish to stay by the car in hopes somebody travels down this rundown road?")
-def choice_1 ():
+def enter_house ():
+   time.sleep(2)
+   print ("you decide nobody is going to travel along the road, especially not at this time")
+   time.sleep(2)
+   print ("as you walk up to the house and step onto it's wooden porch")
+   time.sleep(2)
+   print ("the wood creaks and groans as though eager for it's next victim")
+   time.sleep(2)
+   print ("you knock on the door, the sound echoing through the empty house like thunder.")
+   time.sleep(2)
+   print ("the door swings open though there was nobody there")
+   time.sleep(2)
+   print ("reluctantly you enter the decrepid house.")
+   time.sleep(2)
+   print ("before you lies a long coridoor with various doors, a set of stairs along the wall heading to the top floor,")
+   time.sleep(2)
+   print ("and a set of stairs leading to the bowels of the house, the bottom near invisible as the darkness encases it.")
+   time.sleep(2)
+   print ("do you wish to go upstairs, go downstairs or walk the coridoor?")
+   path_choice = input ("please enter go upstairs, go downstairs or walk coridoor")
+   if path_choice == "go upstairs":
+      upstairs ():
+   elif path_choice == "go downstairs":
+
+    def choice_1 ():
      path_choice = input ("please enter, enter the house or stay by car: ")
      if path_choice == "stay by car":
       time.sleep(2)
@@ -46,14 +55,27 @@ def choice_1 ():
       elif path_choice == "n" or " N":
         print ("thank you for playing")
      elif path_choice == "enter the house":
-      #enter_house()
-      print ("hello")
+      enter_house()
      else:
       print ("please enter enter the house or stay by car: ")
       choice_1()
     
-startgame()
-choice_1()
+def startgame():
+    time.sleep(2)
+    print ("you are travelling down a road in the dead of night.")
+    time.sleep(2)
+    print ("besides you is a forest that stretches as far as the eye can see.")
+    time.sleep(2)
+    print ("suddenly your car breaks down and you quickly get out as smoke pours out of the front of the car.")
+    time.sleep(2)
+    print ("looking around you spot a derelict house.")
+    time.sleep (2)
+    print ("you see only 2 options for what to do in this situation.")
+    time.sleep (2)
+    print ("do you wish to enter the house in hopes of finding help for your predicament?")
+    time.sleep(2)
+    print ("or do you wish to stay by the car in hopes somebody travels down this rundown road?")
+
 
 def print_pause(message):
     print(message)
@@ -109,3 +131,5 @@ def play_again():
             break
  
 play_again()
+
+startgame()
